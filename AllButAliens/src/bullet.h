23be3b64027df1abbfe7b6c57f978002b01ddc1b
Unknown <michael77commins@gmail.com>
2018-12-19 16:54:40 -0800

    Cleaned up duplicated files

--- AllButAliens/src/bullet.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-struct bullet* bullet_construct(float x, float y);
-void bullet_set(struct bullet* bullet, float shipX, float shipY, ofSoundPlayer pew);
-void bullet_move(struct bullet* bullet);
-void bullet_draw(struct bullet* bullet);
--- AllButAliens/src/star.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-struct star* star_construct();
-void star_move(struct star* star);
-void star_draw(struct star* star);
